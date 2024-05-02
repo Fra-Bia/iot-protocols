c7b2e5d2aad0bee4ae61a51a1e4034229efbac09
fai il bravo <francesco.biancifiori@stud.itsaltoadriatico.it>
2024-05-02 17:48:54 +0200

    add Amqp protocol

---
+++ receiver-Amqp/Main/Program.cs
@@ -0,0 +1,15 @@
+﻿string hostName = "goose.rmq2.cloudamqp.com";
+string vHost = "vbjpzwcd";
+string user = "vbjpzwcd";
+string psw = "dFP7zLZEd8Fll6h32fdvgyBQeoby3Vb5";
+string queueName = "queue";
+
+var receiver = new RabbitMQReceiver(hostName, vHost, user, psw);
+receiver.StartReceiving(queueName);
+
+Console.WriteLine(" Press [enter] to exit.");
+Console.ReadLine();
+
+receiver.CloseConnection();
+
+while (true) { }
---
+++ receiver-Amqp/Main/receiver.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+public class RabbitMQReceiver
+{
+    private readonly IConnection _connection;
+    private readonly IModel _channel;
+
+    public RabbitMQReceiver(string host, string user, string vHost, string psw)
+    {
+        var factory = new ConnectionFactory { HostName = host, UserName = user, VirtualHost = vHost, Password = psw };
+        _connection = factory.CreateConnection();
+        _channel = _connection.CreateModel();
+    }
+
+    public void StartReceiving(string queueName)
+    {
+        _channel.QueueDeclare(queue: queueName,
+                              durable: false,
+                              exclusive: false,
+                              autoDelete: false,
+                              arguments: null);
+
+        Console.WriteLine(" [*] Waiting for messages.");
+
+        var consumer = new EventingBasicConsumer(_channel);
+        consumer.Received += (model, ea) =>
+        {
+            var body = ea.Body.ToArray();
+            var message = Encoding.UTF8.GetString(body);
+            Console.WriteLine($" [x] Received {message}");
+        };
+        _channel.BasicConsume(queue: queueName,
+                              autoAck: true,
+                              consumer: consumer);
+    }
+
+    public void CloseConnection()
+    {
+        _channel.Close();
+        _connection.Close();
+    }
+}
+
